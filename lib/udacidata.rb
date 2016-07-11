ad2f5513e60727abda6c7866ef3affe9d9e8c662
Jennifer Swartz <swartzjenn@gmail.com>
2016-07-11 03:59:10 -0400

    Initial commit

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,5 +3,13 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
-  # Your code goes here!
+  def self.create(attributes = nil)
+    
+    #If the object's data is already in the database
+    #create the object
+    # return the object
+    # save the data in the database
+    # return the object
+  end
+  
 end
