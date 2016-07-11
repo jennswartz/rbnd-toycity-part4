d5aa387d49ccb8077520a2f1704a2607cccba159
Jennifer Swartz <swartzjenn@gmail.com>
2016-07-11 07:47:25 -0400

    Add create feature in Udacidata

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -3,13 +3,19 @@ require_relative 'errors'
 require 'csv'
 
 class Udacidata
+
+  @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
+
   def self.create(attributes = nil)
-    
-    #If the object's data is already in the database
-    #create the object
-    # return the object
-    # save the data in the database
-    # return the object
+    if attributes[:id]
+      id = attributes[:id]
+      item = self.new(attributes)
+      return item
+    end
+    item = self.new(attributes)
+    CSV.open(@@data_path, "a+b") do |csv|
+      csv << ["#{item.id}", "#{item.brand}", "#{item.name}", "#{item.price}"]
+    end
+    return item
   end
-  
 end
