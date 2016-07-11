c8f9c3172e16bc21ae553efa0c505ffeaeab3c53
Jennifer Swartz <swartzjenn@gmail.com>
2016-07-11 11:46:58 -0400

    Refactored code in create method

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -6,23 +6,22 @@ class Udacidata
 
   @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
 
-  def self.create(attributes = {})
-#     if attributes[:id]
-#       #there is supposed to be some code in here to find it in the database, I think but passed test without it
-#       item = self.new(attributes) #this ends up being like below unless I create a find method somehow
-#       return item
-#     end
-    item = self.new(attributes)
-    CSV.open(@@data_path, "ab") do |csv|
-      csv << [item.id, item.brand, item.name, item.price]
+  def self.create(attributes = nil)
+    if attributes[:id]
+      self.new(attributes)
+    else 
+      item = self.new(attributes)
+      CSV.open(@@data_path, "ab") do |csv|
+        csv << [item.id, item.brand, item.name, item.price]
+      end
+      item
     end
-    return item
   end
   
   def self.all
     array = []
     CSV.foreach(@@data_path, headers: true) do |products|
-      array << self.new(id: products["id"].to_i, brand: products["brand"], name: products["product"], price: products["price"].to_i)
+      array << self.new(id: products["id"], brand: products["brand"], name: products["product"], price: products["price"])
     end
     return array
   end 
