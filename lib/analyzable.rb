2bc50d2a23df44a1c4535b4a23c7c38f6a99f435
Jennifer Swartz <swartzjenn@gmail.com>
2016-07-12 07:22:47 -0400

    Add method for total average price

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -2,24 +2,11 @@ module Analyzable
   
   @@data_path = File.dirname(__FILE__) + "/../data/data.csv"
   
-  #accept an array of product objects and return the average price.  Average price is add up the 
-  #prices and divide by the number of prices you added together
-  def average_price(options = nil)
-    sum = Product.create["sum.brand"].inject(0, :+)
-    total = (sum.price.to_f / sum.price.length).round(2)
+  # Accept an array of product objects and return the average price.  Array is return (i.e.
+  # the selection) of the Product.where method.  (Ave price can be totalled by brand or name of product) 
+  def average_price(selection)
+    total_price = selection.inject(0) { |total, product| total + product.price }
+    average_price = total_price / selection.length
+    return average_price.round(2)
   end
 end
-
-# def calc_total_brand_price(brand)
-#   $products_hash["items"].inject(0) { |total, product| product["brand"] == brand ?
-#   		total + product["full-price"].to_f : total }
-# end
-# 
-# def calc_ave_brand_price(brand, total_price)
-#   brand_products = $products_hash["items"].select { |product| product["brand"] == brand }
-#   return (total_price / brand_products.length).round(2)
-# end
-
-
-  
- 
