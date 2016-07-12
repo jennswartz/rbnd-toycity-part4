c65330d8fa5ddb71b80b00a63de6c84816d79b77
Jennifer Swartz <swartzjenn@gmail.com>
2016-07-12 01:38:43 -0400

    Prepare for analyzable methods

--- lib/analyzable.rb
+++ lib/analyzable.rb
@@ -1,3 +1,26 @@
 module Analyzable
-  # Your code goes here!
+  
+
+  def total_price(brand)
+    Product.where(brand: "AverageBrand").inject(0) { |total, product| product["brand"] == brand ? total + product["full-price"].to_f : total }
+  end
+  
+  def average_price(brand, total_price)
+    brand_product = Brand.where(brand: "AverageBrand").select { |product| product["brand"] == brand }
+    return (total_price / brand_products.length).round(2)
+  end
+end
+    
+
+
+
+  
+#   def average_price(self.all, total_price)
+#     brand_products = self.all.select { |product| product["brand"] == brand }
+#     return (total_price / brand_products.length).round(2)
+#   end
+  
+  
+
+
 end
