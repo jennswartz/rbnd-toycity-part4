4d226088cb022d74d5c1d8fcf0287fa4009a6b9e
Jennifer Swartz <swartzjenn@gmail.com>
2016-07-13 07:48:53 -0400

    Refactor udacidata.rb

--- lib/udacidata.rb
+++ lib/udacidata.rb
@@ -10,7 +10,7 @@ class Udacidata
     check_csv = CSV.open(@@data_path, headers:true, converters: :all)
     item = self.new(attributes)
     check_csv.each do |row|
-      return item if row['id'] == item.id
+      return item if row["id"] == item.id
     end
     CSV.open(@@data_path, "a+") do |csv|
       csv << [item.id, item.brand, item.name, item.price]
@@ -39,7 +39,7 @@ class Udacidata
   def self.find(id)
     result = self.all.find { |product| product.id == id }
     unless result 
-      raise ProductNotFoundError, "No product found with that id."
+      raise ProductNotFoundError, "No product has that id."
     end
     result
   end
@@ -53,14 +53,7 @@ class Udacidata
   end
   
   def self.where(value={})
-    selection = []
-    if value[:brand]
-      selection = self.all.select { |product| product.brand == value[:brand] } 
-    elsif value[:name]
-      selection = self.all.select { |product| product.name == value[:name] }
-    end
-    selection
-  #self.all.select { |product| value[:brand] == product.brand || value[:name] == product.name }
+    self.all.select { |product| value[:brand] == product.brand || value[:name] == product.name }
   end
   
   def update(attributes = {}) 
